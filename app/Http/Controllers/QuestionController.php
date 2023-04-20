eaf69d4c2586a4865b910ccd8d99570ce280d06a
rex <j.wong@hitowerit.com>
2023-04-20 12:53:29 +0800

    Question One Done

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\QuestionOneRequest;
+
+class QuestionController extends Controller
+{
+    public function QuestionOne(QuestionOneRequest $request){
+        $value = $request['value'];
+
+        $arr = array();
+
+        foreach (range(1, $value) as $i) {
+            if ($i % 3 == 0 && $i %  5 == 0) {
+                array_push($arr, 'fizzbuzz');
+            } else if ($i %  5 == 0) {
+                array_push($arr, 'buzz');
+            } else if ($i % 3 == 0) {
+                array_push($arr, 'fizz');
+            } else {
+                array_push($arr, $i);
+            }
+        }
+
+        return $arr;
+    }
+}
